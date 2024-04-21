b36beca1c435997f50f4e1eb8b2310ce3cc7cd5c
SrdjanV <SrdjanV@protonmail.com>
2024-04-21 22:32:46 +0200

    Add more overloaded methods to the hotswap extension

--- plugin/src/main/java/io/github/srdjanv/hotswapgradle/extentions/HotSwapExtension.java
+++ plugin/src/main/java/io/github/srdjanv/hotswapgradle/extentions/HotSwapExtension.java
@@ -4,17 +4,15 @@ import groovy.lang.GroovyObjectSupport;
 import io.github.srdjanv.hotswapgradle.HotSwapGradleService;
 import io.github.srdjanv.hotswapgradle.dcvm.DcevmSpec;
 import io.github.srdjanv.hotswapgradle.resolver.*;
+import javax.inject.Inject;
 import org.gradle.api.Action;
 import org.gradle.api.Project;
-import org.gradle.api.plugins.JavaPlugin;
-import org.gradle.api.plugins.JavaPluginExtension;
+import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.services.BuildServiceRegistration;
 import org.gradle.api.tasks.JavaExec;
 import org.gradle.api.tasks.TaskProvider;
 import org.gradle.jvm.toolchain.*;
-
-import javax.inject.Inject;
 
 public abstract class HotSwapExtension extends GroovyObjectSupport {
     private final Project project;
@@ -23,10 +21,9 @@ public abstract class HotSwapExtension extends GroovyObjectSupport {
     @Inject
     public HotSwapExtension(Project project) {
         this.project = project;
-        project.getPlugins().apply(JavaPlugin.class);
+        project.getPlugins().apply(JavaBasePlugin.class);
 
-        BuildServiceRegistration<?, ?> serviceRegistration = project
-                .getGradle()
+        BuildServiceRegistration<?, ?> serviceRegistration = project.getGradle()
                 .getSharedServices()
                 .getRegistrations()
                 .getByName(HotSwapGradleService.class.getName());
@@ -35,28 +32,22 @@ public abstract class HotSwapExtension extends GroovyObjectSupport {
     }
 
     public void configureTask(TaskProvider<? extends JavaExec> taskProvider) {
-        taskProvider.configure(javaExec ->
-                configureTask(
-                        javaExec,
-                        project,
-                        spec -> {
-                        }));
+        taskProvider.configure(javaExec -> configureTask(javaExec, project, spec -> {}));
     }
 
-    public void configureTask(
-            TaskProvider<? extends JavaExec> taskProvider,
-            Action<? super DcevmSpec> spec) {
-        taskProvider.configure(javaExec ->
-                configureTask(
-                        javaExec,
-                        project,
-                        spec));
+    public void configureTask(TaskProvider<? extends JavaExec> taskProvider, Action<? super DcevmSpec> spec) {
+        taskProvider.configure(javaExec -> configureTask(javaExec, project, spec));
     }
 
-    public void configureTask(
-            JavaExec task,
-            Project project,
-            Action<? super DcevmSpec> spec) {
+    public void configureTask(JavaExec task) {
+        configureTask(task, project, spec -> {});
+    }
+
+    public void configureTask(JavaExec task, Action<? super DcevmSpec> spec) {
+        configureTask(task, project, spec);
+    }
+
+    public void configureTask(JavaExec task, Project project, Action<? super DcevmSpec> spec) {
         configureTask(
                 task,
                 IDcevmSpecResolver.of(project),
@@ -79,11 +70,16 @@ public abstract class HotSwapExtension extends GroovyObjectSupport {
             var hotswapService = hotSwapGradleService.get();
             var dcevmSpec = specResolver.resolveDcevmSpec(hotswapService, spec);
 
-            JavaLauncher javaLauncher = hotswapService.getKnownDCEVMRegistry().locateVM(launcherResolver, specResolver, dcevmSpec);
+            JavaLauncher javaLauncher =
+                    hotswapService.getKnownDCEVMRegistry().locateVM(launcherResolver, specResolver, dcevmSpec);
             if (javaLauncher == null)
-                javaLauncher = hotswapService.getCashedJVMRegistry().locateVM(metadataResolver, metadataLauncherResolver, dcevmSpec);
+                javaLauncher = hotswapService
+                        .getCashedJVMRegistry()
+                        .locateVM(metadataResolver, metadataLauncherResolver, dcevmSpec);
             if (javaLauncher == null)
-                javaLauncher = hotswapService.getLocalJVMRegistry().locateVM(jvmResolver, metadataResolver, metadataLauncherResolver, dcevmSpec);
+                javaLauncher = hotswapService
+                        .getLocalJVMRegistry()
+                        .locateVM(jvmResolver, metadataResolver, metadataLauncherResolver, dcevmSpec);
 
             if (javaLauncher == null) {
                 javaLauncher = hotswapService.getLocalJVMRegistry().locateVanillaVM(launcherResolver, dcevmSpec);
