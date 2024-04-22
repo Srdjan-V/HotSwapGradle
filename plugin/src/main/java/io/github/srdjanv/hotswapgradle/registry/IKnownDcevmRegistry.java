795b862f5a7854ca5e4af6e2086100fa944f18a0
SrdjanV <SrdjanV@protonmail.com>
2024-04-22 18:53:56 +0200

    Update known dcevm vendors

--- plugin/src/main/java/io/github/srdjanv/hotswapgradle/registry/IKnownDcevmRegistry.java
+++ plugin/src/main/java/io/github/srdjanv/hotswapgradle/registry/IKnownDcevmRegistry.java
@@ -3,59 +3,87 @@ package io.github.srdjanv.hotswapgradle.registry;
 import io.github.srdjanv.hotswapgradle.dcvm.DcevmSpec;
 import io.github.srdjanv.hotswapgradle.locators.IDcevmLocator;
 import io.github.srdjanv.hotswapgradle.suppliers.KnownDcevmSupplier;
+import java.util.*;
+import java.util.function.Consumer;
 import org.gradle.api.Action;
 import org.gradle.api.JavaVersion;
 import org.gradle.jvm.toolchain.JavaLanguageVersion;
 import org.gradle.jvm.toolchain.JvmVendorSpec;
 
-import java.util.*;
-import java.util.function.Consumer;
-
 public interface IKnownDcevmRegistry extends IDcevmLocator {
 
     default void setDefaultRegistry() {
         configureRegistry(Map::clear);
+        // linux only
+        populateRegistry(
+                JavaVersion.VERSION_1_8,
+                () -> Collections.singletonList(dcevmSpec -> {
+                    dcevmSpec.getLanguageVersion().set(JavaLanguageVersion.of(8));
+                    dcevmSpec.getVendor().set(JvmVendorSpec.matching("trava"));
+                }));
+
         populateRegistry(JavaVersion.VERSION_11, () -> {
             List<Action<? super DcevmSpec>> jdk11 = new ArrayList<>();
-            //linux only
+            jdk11.add(dcevmSpec -> {
+                dcevmSpec.getLanguageVersion().set(JavaLanguageVersion.of(11));
+                dcevmSpec.getVendor().set(JvmVendorSpec.JETBRAINS);
+                dcevmSpec
+                        .getArguments()
+                        .set(Arrays.asList(
+                                "-XX:+AllowEnhancedClassRedefinition",
+                                String.format(
+                                        "-javaagent:%s",
+                                        dcevmSpec
+                                                .getAgentJar()
+                                                .get()
+                                                .getAsFile()
+                                                .getAbsolutePath())));
+            });
+
             jdk11.add(dcevmSpec -> {
                 dcevmSpec.getLanguageVersion().set(JavaLanguageVersion.of(11));
                 dcevmSpec.getVendor().set(JvmVendorSpec.matching("trava"));
             });
 
-            jdk11.add(dcevmSpec -> {
-                dcevmSpec.getLanguageVersion().set(JavaLanguageVersion.of(11));
-                dcevmSpec.getVendor().set(JvmVendorSpec.JETBRAINS);
-                dcevmSpec.getArguments().set(
-                        Arrays.asList("-XX:+AllowEnhancedClassRedefinition",
-                                String.format("-javaagent:%s",
-                                        dcevmSpec.getAgentJar().get().getAsFile().getAbsolutePath())
-                        ));
-            });
             return jdk11;
         });
 
+        populateRegistry(
+                JavaVersion.VERSION_17,
+                () -> Collections.singletonList(dcevmSpec -> {
+                    dcevmSpec.getLanguageVersion().set(JavaLanguageVersion.of(17));
+                    dcevmSpec.getVendor().set(JvmVendorSpec.JETBRAINS);
 
-        populateRegistry(JavaVersion.VERSION_17, () -> Collections.singletonList(dcevmSpec -> {
-            dcevmSpec.getLanguageVersion().set(JavaLanguageVersion.of(17));
-            dcevmSpec.getVendor().set(JvmVendorSpec.JETBRAINS);
+                    dcevmSpec
+                            .getArguments()
+                            .set(Arrays.asList(
+                                    "-XX:+AllowEnhancedClassRedefinition",
+                                    String.format(
+                                            "-javaagent:%s",
+                                            dcevmSpec
+                                                    .getAgentJar()
+                                                    .get()
+                                                    .getAsFile()
+                                                    .getAbsolutePath())));
+                }));
 
-            dcevmSpec.getArguments().set(
-                    Arrays.asList("-XX:+AllowEnhancedClassRedefinition",
-                            String.format("-javaagent:%s",
-                                    dcevmSpec.getAgentJar().get().getAsFile().getAbsolutePath())
-                    ));
-        }));
-
-        populateRegistry(JavaVersion.VERSION_21, () -> Collections.singletonList(dcevmSpec -> {
-            dcevmSpec.getLanguageVersion().set(JavaLanguageVersion.of(21));
-            dcevmSpec.getVendor().set(JvmVendorSpec.JETBRAINS);
-            dcevmSpec.getArguments().convention(
-                    Arrays.asList("-XX:+AllowEnhancedClassRedefinition",
-                            String.format("-javaagent:%s",
-                                    dcevmSpec.getAgentJar().get().getAsFile().getAbsolutePath())
-                    ));
-        }));
+        populateRegistry(
+                JavaVersion.VERSION_21,
+                () -> Collections.singletonList(dcevmSpec -> {
+                    dcevmSpec.getLanguageVersion().set(JavaLanguageVersion.of(21));
+                    dcevmSpec.getVendor().set(JvmVendorSpec.JETBRAINS);
+                    dcevmSpec
+                            .getArguments()
+                            .convention(Arrays.asList(
+                                    "-XX:+AllowEnhancedClassRedefinition",
+                                    String.format(
+                                            "-javaagent:%s",
+                                            dcevmSpec
+                                                    .getAgentJar()
+                                                    .get()
+                                                    .getAsFile()
+                                                    .getAbsolutePath())));
+                }));
     }
 
     void populateRegistry(JavaVersion javaVersion, KnownDcevmSupplier knownDCEVMSupplier);
