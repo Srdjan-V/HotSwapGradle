d1ab7d2f292301643690454435f19ab3f1d44440
SrdjanV <SrdjanV@protonmail.com>
2024-04-25 19:04:28 +0200

    Make the DefaultJVMResolver use tge global validator

--- plugin/src/main/java/io/github/srdjanv/hotswapgradle/resolver/internal/DefaultJVMResolver.java
+++ plugin/src/main/java/io/github/srdjanv/hotswapgradle/resolver/internal/DefaultJVMResolver.java
@@ -1,19 +1,24 @@
 package io.github.srdjanv.hotswapgradle.resolver.internal;
 
-import io.github.srdjanv.hotswapgradle.dcevmdetection.legacy.LegacyDcevmDetection;
+import io.github.srdjanv.hotswapgradle.HotswapGradleService;
 import io.github.srdjanv.hotswapgradle.dcvm.DcevmMetadata;
 import io.github.srdjanv.hotswapgradle.resolver.IDcevmMetadataResolver;
 import io.github.srdjanv.hotswapgradle.resolver.IJVMResolver;
+import io.github.srdjanv.hotswapgradle.util.JavaUtil;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
+import org.gradle.api.Project;
 import org.gradle.internal.jvm.inspection.JavaInstallationRegistry;
 
 public class DefaultJVMResolver implements IJVMResolver {
     private final JavaInstallationRegistry registry;
+    private final HotswapGradleService service;
 
     @Inject
-    public DefaultJVMResolver(JavaInstallationRegistry registry) {
+    public DefaultJVMResolver(Project project, JavaInstallationRegistry registry) {
+        service =
+                JavaUtil.getHotswapExtension(project).getHotswapGradleService().get();
         this.registry = registry;
     }
 
@@ -21,7 +26,7 @@ public class DefaultJVMResolver implements IJVMResolver {
     public List<DcevmMetadata> getAllDcevmToolchains(IDcevmMetadataResolver metadataResolver) {
         return registry.toolchains().stream()
                 .filter(tool -> tool.metadata.isValidInstallation())
-                .filter(tool -> LegacyDcevmDetection.isPresent(tool.metadata.getJavaHome()))
+                .filter(tool -> service.getDcevmValidator().validateDcevm(tool.metadata.getJavaHome()))
                 .map(tool -> metadataResolver.resolveDcevmMetadata(tool.metadata.getJavaHome()))
                 .collect(Collectors.toList());
     }
